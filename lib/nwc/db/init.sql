f8462bb0f10dad6ef4c53dbd7b5ad2fa1cb1fc13
Braydon <courier@braydon.com>
2026-08-17 15:18:33 -0700

    Add index for nwc database.

--- lib/nwc/db/init.sql
+++ lib/nwc/db/init.sql
@@ -3,3 +3,4 @@ CREATE UNIQUE INDEX IF NOT EXISTS `idx_request_events_nostr_id` ON `request_even
 CREATE TABLE IF NOT EXISTS "response_events" (`id` integer,`nostr_id` text UNIQUE,`request_nostr_id` text,`user` text,`pub_key` text,`raw` text,`status` text,`created_at` datetime,`updated_at` datetime,PRIMARY KEY (`id`));
 CREATE UNIQUE INDEX IF NOT EXISTS `idx_response_events_nostr_id` ON `response_events`(`nostr_id`);
 CREATE UNIQUE INDEX IF NOT EXISTS `idx_response_events_request_nostr_id` ON `response_events`(`request_nostr_id`);
+CREATE INDEX IF NOT EXISTS `idx_request_events_user_status` ON `request_events`(`user`, `status`);
